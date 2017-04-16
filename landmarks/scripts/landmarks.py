1042ca7b5a82c13279c704ef975ae2385932e811
Jared Greene <jmg2227@columbia.edu>
2017-04-16 18:39:27 -0400

    small demo made

--- landmarks/scripts/landmarks.py
+++ landmarks/scripts/landmarks.py
@@ -3,4 +3,36 @@
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-cl
+
+
+class MoveBaseClient(object):
+    def __init__(self):
+        self.client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("waiting for movebase client")
+        self.client.wait_for_server()
+
+    def goto(self, x, y, theta, frame="map"):
+         move_goal = MoveBaseGoal()
+         move_goal.target_pose.pose.position.x = x
+         move_goal.target_pose.pose.position.y = y
+         move_goal.target_pose.pose.orientation.z = sin(theta/2.0)
+         move_goal.target_pose.pose.orientation.w = cos(theta/2.0)
+         move_goal.target_pose.header.frame_id = frame
+         move_goal.target_pose.header.stamp = rospy.Time.now()
+
+	self.client.send_goal(move_goal)
+	self.client.wait_for_result()
+
+if __name__ == "__main__":
+     # Create a node
+     rospy.init_node("landmarks")
+
+     # Make sure sim time is working
+     while not rospy.Time.now():
+         pass
+
+     # Setup client
+     move_base = MoveBaseClient()
+
+     move_base.goto(5.0, 2.1, 0.0)
+     move_base.goto(0.0, 5.0, 0.5)
